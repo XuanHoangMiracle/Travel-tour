e353f5efdd6917ba6695f3ee05f86dff8299c7b3
xdTruongThanhPhat <hoang_2151220244@dau.edu.vn>
2025-09-15 17:17:20 +0700

    Home Login

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { assets, cities } from '../assets/assets';
+const Hero = () => {
+  return (
+    <div className='flex flex-col items-start justify-center px-6
+    md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/HeroImage.png")] 
+    bg-no-repeat bg-cover bg-center h-screen'> 
+        <p className='bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20'>Trải nghiệm du lịch tuyệt vời </p>
+        <h1 className='font-playfair text-2xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4'>Điểm Đến Hoàn Hảo Dành Cho Bạn</h1>
+        <p className='max-w-130 mt-2 text-sm md:text-base'>Khám phá những địa điểm du lịch tuyệt đẹp và trải nghiệm những dịch vụ tốt nhất cùng chúng tôi.</p>
+
+         <form className='bg-white text-gray-500 rounded-lg px-6 py-4 mt-8 flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
+
+            <div>
+                <div className='flex items-center gap-2'>
+                    <img src={assets.location} alt="" className='h-4'/>
+                    <label htmlFor="destinationInput">Chọn điểm đến</label>
+                </div>
+                <input list='destinations' id="destinationInput" type="text" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" placeholder="Điểm đến của bạn" required />
+                <datalist id='destinations'>
+                    {cities.map((city,index)=>(
+                        <option value={city} key={index}>{city}</option>
+                    ))}
+                </datalist>
+            </div>
+
+            <div>
+                <div className='flex items-center gap-2'>
+                     <img src={assets.calenderIcon} alt="" className='h-4'/>
+                    <label htmlFor="checkIn">Ngày khởi hành</label>
+                </div>
+                <input id="checkIn" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
+            </div>
+
+            <div>
+                <div className='flex md:flex-col max-md:gap-2 max-md:items-center'>
+                <label htmlFor="days">Số Ngày</label>
+                <input min={1} max={30} id="days" type="number" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none  max-w-16" placeholder="0" />
+            </div>
+            </div>
+
+            <div className='flex md:flex-col max-md:gap-2 max-md:items-center'>
+                <label htmlFor="guests">Số khách</label>
+                <input min={1} max={20} id="guests" type="number" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none  max-w-16" placeholder="0" />
+            </div>
+
+            <button className='flex items-center justify-center gap-1 rounded-md bg-black py-3 px-4 text-white my-auto cursor-pointer max-md:w-full max-md:py-1' >
+                 <img src={assets.searchIcon} alt="searchIcon" className='h-7'/>
+                <span>Tìm kiếm</span>
+            </button>
+        </form>
+    </div>
+  )
+}
+
+export default Hero
