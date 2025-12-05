13afeffdd224b361431005cde1661959ce223a06
XuanHoang <xuanhoangmk4@gmail.com>
2025-12-05 22:09:30 +0700

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useUser, useAuth } from "@clerk/clerk-react";
 import toast, { Toaster } from "react-hot-toast";
 
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || "https://travelserver-oekt.onrender.com";
 
 const AppContext = createContext();
 
